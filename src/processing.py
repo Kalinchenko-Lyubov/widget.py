52bf248a9db2dbf63e224d9f54ff4121d5cbc5b4
Luybov <kalna.lyubov2017@yandex.ru>
2026-01-02 02:01:40 +0300

    add file with the function filter_by_state

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,5 @@
 def filter_by_state(list_state: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """возвращает список словарей с указанным значением ключа  state"""
     filter_lists = []
     for el in list_state:
         if el["state"] == state:
