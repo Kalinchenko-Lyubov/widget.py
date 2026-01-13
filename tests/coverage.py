f68fda89120399617fff137bf67970908f7f04a1
Luybov <kalna.lyubov2017@yandex.ru>
2026-01-13 17:52:56 +0300

    add fixture, fixed tests for masks.py

---
+++ tests/coverage.py
@@ -0,0 +1,31 @@
+import pytest
+
+@pytest.fixture
+def example_num_card_invalid():
+    return ["aaaabbbbccccdddd",
+        "ааааббббввввгггг",
+        'a ,."%#@!&*()/*[]{}',
+        "111122223333",
+        "11112222333344445555",
+        "",
+        " ",
+        "123фыв123 asd-",
+        "111122223333asfg",
+        "123",
+        "123фыв123 asd-",
+        "1111-2222-3333-as-fg"
+            ]
+
+@pytest.fixture
+def example_num_account_invalid():
+    return ["aaaabbbbccccdddd",
+        "ааааббббввввгггг",
+        'a ,."%#@!&*()/*[]{}',
+        "",
+        " ",
+        "123фыв123 asd-",
+        "111122223333asfg",
+        "123",
+        "123фыв123 asd-",
+        "1111-2222-3333-as-fg"
+            ]
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,6 +1,7 @@
 import pytest
 
 from src.masks import get_mask_card_number, get_mask_account
+from tests.coverage import example_num_card_invalid, example_num_account_invalid
 
 
 @pytest.mark.parametrize(
@@ -8,29 +9,19 @@ from src.masks import get_mask_card_number, get_mask_account
     [("1111222233334444", "1111 22** **** 4444"), ("0000000000000000", "0000 00** **** 0000")],
 )
 def test_get_mask_card_number(number_card, mask_card):
+    """Тестируем случаи верного ввода номера карты"""
     assert get_mask_card_number(number_card) == mask_card
 
 
-@pytest.mark.parametrize(
-    "number_card",
-    [
-        "aaaabbbbccccdddd",
-        "ааааббббввввгггг",
-        'a ,."%#@!&*()/*[]{}',
-        "111122223333",
-        "11112222333344445555",
-        "",
-        " ",
-        "123фыв123 asd-",
-        "111122223333asfg",
-    ],
-)
-def test_get_mask_card_number_invalid(number_card):
-    with pytest.raises(ValueError):
-        get_mask_card_number(number_card)
+def test_get_mask_card_number_invalid(example_num_card_invalid):
+    """Тестируем ошибочные случаи ввода номера карты"""
+    for num_card in example_num_card_invalid:
+        with pytest.raises(ValueError):
+            get_mask_card_number(num_card)
 
 
 def test_get_mask_card_number_letters(number_card="123фыв123 asd-"):
+    """Проверяем, правильность сообщения об ошибке"""
     with pytest.raises(
         ValueError,
         match="Неверный номер карты. Проверьте количество цифр, их должно быть 16.\n"
@@ -47,25 +38,15 @@ def test_get_mask_account(user_account, mask_account):
     assert get_mask_account(user_account) == mask_account
 
 
-@pytest.mark.parametrize(
-    "user_account",
-    [
-        "aaaabbbbccccddddeeee",
-        "ааааббббввввггггдддд",
-        "a ,.'%#@!&*()/*[]{}",
-        "",
-        "123",
-        " "
-        "123фыв123 asd-",
-        "1111-2222-3333-as-fg",
-    ],
-)
-def test_get_mask_account_invalid(user_account):
-    with pytest.raises(ValueError):
-        get_mask_account(user_account)
+def test_get_mask_account_invalid(example_num_account_invalid):
+    """Тестируем ошибочные случаи ввода номера счета"""
+    for num_account in example_num_account_invalid:
+        with pytest.raises(ValueError):
+            get_mask_account(num_account)
 
 
 def test_get_mask_account_letters(user_account="123 фыв 123 asd- 1234"):
+    """Проверяем, правильность сообщения об ошибке"""
     with pytest.raises(
         ValueError,
         match="Номер счета состоит только из цифр",
