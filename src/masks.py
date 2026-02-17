8857b9471ca8872913a603a9564a5b0f2180b767
Luybov <kalna.lyubov2017@yandex.ru>
2026-02-18 00:44:46 +0300

    fix masks.py and utils.py

--- src/masks.py
+++ src/masks.py
@@ -1,11 +1,15 @@
 import logging
+import os
 from typing import Union
 
-logger = logging.getLogger('masks')
+os.chdir("..")
+if not os.path.exists("logs"):
+    os.makedirs("logs")
+
+logger = logging.getLogger("masks")
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler('logs.log', mode='w')
-file_formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s: %(message)s')
-file_handler = logging.FileHandler('logs.log', encoding='utf-8')
+file_handler = logging.FileHandler("logs/masks_logs.log", mode="w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 
@@ -13,32 +17,32 @@ logger.addHandler(file_handler)
 def get_mask_card_number(number_card: Union[int, str]) -> str:
     """Маскирует номер банковской карты: возвращает первые 6 и последние 4 цифры, между ними *"""
 
-    logger.info('Приступаем к маскировке банковской карты')
+    logger.info("Приступаем к маскировке банковской карты")
     if len(str(number_card)) != 16:
-        logger.error('Ошибка введения данных: введено неверное количество цифр')
+        logger.error("Ошибка введения данных: введено неверное количество цифр")
         raise ValueError("Неверный номер карты. Проверьте количество цифр, их должно быть 16")
 
     if not str(number_card).isdigit():
-        logger.error('Ошибка введения данных: введены неверные символы')
+        logger.error("Ошибка введения данных: введены неверные символы")
         raise ValueError("Номер карты состоит только из цифр")
 
     mask_card_num = f"{str(number_card)[:4]} {str(number_card)[4:6]}** **** {str(number_card)[-4:]}"
-    logger.info('Успешное выполнение маскировки банковской карты')
+    logger.info("Успешное выполнение маскировки банковской карты")
     return mask_card_num
 
 
 def get_mask_account(user_account: Union[int, str]) -> str:
     """Маскирует номер банковского счета: принимает число, возвращает ** и 4 последние цифры"""
 
-    logger.info('Приступаем к маскировке банковского счета')
+    logger.info("Приступаем к маскировке банковского счета")
     if not str(user_account).isdigit():
-        logger.error('Ошибка введения данных: введены неверные символы')
+        logger.error("Ошибка введения данных: введены неверные символы")
         raise ValueError("Номер счета состоит только из цифр")
 
     if len(str(user_account)) < 4:
-        logger.error('Ошибка введения данных: введено неверное количество цифр')
+        logger.error("Ошибка введения данных: введено неверное количество цифр")
         raise ValueError("Номер счета должен содержать не менее 4 цифр")
 
     mask_account = f"**{str(user_account)[-4:]}"
-    logger.info('Успешное выполнение маскировки банковского счета')
+    logger.info("Успешное выполнение маскировки банковского счета")
     return mask_account
--- src/utils.py
+++ src/utils.py
@@ -1,53 +1,40 @@
 import json
+import logging
 import os
 from typing import Any, Dict, List
-import logging
 
-logger = logging.getLogger('utils')
+os.chdir("..")
+if not os.path.exists("logs"):
+    os.makedirs("logs")
+
+logger = logging.getLogger("utils")
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler('logs.log', mode='w')
-file_formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s: %(message)s')
-file_handler = logging.FileHandler('logs.log', encoding='utf-8')
+file_handler = logging.FileHandler("logs/utils_logs.log", mode="w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 
 
 def get_transaction_data(file_path: str) -> List[Dict[str, Any]] | bool:
     """Возвращает данные о финансовых транзакциях"""
-    logger.info('Выполняем проверку наличия информации о финансовых транзакциях')
+    logger.info("Выполняем проверку наличия информации о финансовых транзакциях")
 
     if not os.path.exists(file_path):
-        logger.warning(f'Файл {file_path} не найден.')
+        logger.error(f"Файл {file_path} не найден.")
         return []
     try:
-        logger.info('Открываем файл с транзакциями')
+        logger.info("Открываем файл с транзакциями")
         with open(file_path, encoding="utf-8") as file:
             transaction_data = json.load(file)
         if isinstance(transaction_data, list):
-            logger.info('Успешный вывод данных с транзакциями')
+            logger.info("Успешный вывод данных с транзакциями")
             return transaction_data
         else:
-            logger.error('Данные в файле не являются списком!')
+            logger.error("Данные в файле не являются списком!")
             return []
 
     except json.JSONDecodeError:
-        logger.error(f'Ошибка декодирования файла {file_path}')
+        logger.error(f"Ошибка декодирования файла {file_path}")
         return []
 
     return []
-
-
-if __name__ == "__main__":
-    print(get_transaction_data(os.path.join(os.path.dirname(__file__), '../data/operations.json')))
-
-
-# Создайте логеры для перечисленных модулей:
-# masks,
-# utils.
-# Реализуйте запись логов в файл. Логи должны записываться в папку
-# logs в корне проекта. Файлы логов должны иметь расширение
-# .log.
-# Формат записи лога в файл должен включать метку времени,
-# название модуля, уровень серьезности и сообщение, описывающее
-# событие или ошибку, которые произошли.
-# Лог должен перезаписываться при каждом запуске приложения.
